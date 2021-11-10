496e1113c5b1dd5cd1bb6b9d0c7e1c1d3b79f89e
o.lelenkov <o.lelenkov@gmail.com>
2021-11-10 09:26:03 +0300

    update nvim-cmp config plugin

--- lua/settings/completion.lua
+++ lua/settings/completion.lua
@@ -13,14 +13,6 @@ function setup()
     vim.o.completeopt = "menuone,noinsert,noselect";
     vim.cmd [[ set shortmess+=c ]]
 
-    local has_words_before = function()
-        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-            return false
-        end
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     local feedkey = function(key, mode)
         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
     end
@@ -38,29 +30,40 @@ function setup()
             end,
         },
         mapping = {
+            ['<C-n>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.mapping.select_next_item({
+                        behavior = cmp.SelectBehavior.Insert,
+                    })(fallback)
+                else
+                    cmp.mapping.complete()(fallback)
+                end
+            end, { "i", "s" }),
             ["<C-p>"] = cmp.mapping.select_prev_item(),
-            ["<C-n>"] = cmp.mapping.complete(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
             ["<C-d>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ['<C-e>'] = cmp.mapping.close(),
-            ["<CR>"] = cmp.mapping.confirm({ select = true }),
             ["<Tab>"] = cmp.mapping(function(fallback)
-                if vim.fn.pumvisible() == 1 then
-                    feedkey("<C-n>", "n")
+                if cmp.visible() then
+                    cmp.mapping.select_next_item({
+                        behavior = cmp.SelectBehavior.Insert,
+                    })(fallback)
                 elseif vim.fn["vsnip#available"]() == 1 then
                     feedkey("<Plug>(vsnip-expand-or-jump)", "")
-                elseif has_words_before() then
-                    cmp.complete()
                 else
                     fallback()
                 end
             end, { "i", "s" }),
-
-            ["<S-Tab>"] = cmp.mapping(function()
-                if vim.fn.pumvisible() == 1 then
-                    feedkey("<C-p>", "n")
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.mapping.select_prev_item({
+                        behavior = cmp.SelectBehavior.Insert,
+                    })(fallback)
                 elseif vim.fn["vsnip#jumpable"](-1) == 1 then
                     feedkey("<Plug>(vsnip-jump-prev)", "")
+                else
+                    fallback()
                 end
             end, { "i", "s" }),
         },

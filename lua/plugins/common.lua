d55c0a96402a6e53c8526e2cbb0fb49178147e66
o.lelenkov <o.lelenkov@gmail.com>
2022-08-22 14:11:56 +0300

    feat: enable FixCursorHold and popup plugins

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -2,8 +2,8 @@
 
 function install(use)
   use("nvim-lua/plenary.nvim");              -- Useful lua functions used ny lots of plugins
-  -- use("nvim-lua/popup.nvim");                -- An implementation of the Popup API from vim in Neovim
-  -- use("antoinemadec/FixCursorHold.nvim");    -- This is needed to fix lsp doc highlight
+  use("nvim-lua/popup.nvim");                -- An implementation of the Popup API from vim in Neovim
+  use("antoinemadec/FixCursorHold.nvim");    -- This is needed to fix lsp doc highlight
   use("kyazdani42/nvim-web-devicons");
   use("lambdalisue/suda.vim");
 end
